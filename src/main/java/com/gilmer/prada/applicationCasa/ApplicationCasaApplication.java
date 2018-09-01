bb14aeaad8ddc4706435f57086834faf66821d6d
gilmer <pradavelasque@gmail.com>
2018-09-01 11:24:47 -0400

    first commit

---
+++ src/main/java/com/gilmer/prada/applicationCasa/ApplicationCasaApplication.java
@@ -0,0 +1,12 @@
+package com.gilmer.prada.applicationCasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationCasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationCasaApplication.class, args);
+	}
+}
